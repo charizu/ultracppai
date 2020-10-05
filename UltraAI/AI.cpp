1c9efeafa384f7245030b91b485957822c8489c2
charizu <66905971+charizu@users.noreply.github.com>
2020-10-05 22:17:07 +0200

    Add files via upload

---
+++ UltraAI/AI.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int height;
+
+int main()
+{
+	cout << "Enter ur height (cm's)" << endl;
+	cin >> height;
+	cout << "this is ur height - " << height << "cm";
+	return 0;
+
+}
